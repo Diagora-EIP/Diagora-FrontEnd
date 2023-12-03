92e8c93c7e71e18b22549d311ba97230e12eba31
Lyne986 <71839652+Lyne986@users.noreply.github.com>
2023-12-04 00:31:39 +0900

    [ADD] Hiding navbar in /login', '/register', '/forgot-password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front';
+  title = 'Diagora';
+  showNavbar: boolean = true;
+
+  constructor(private router: Router) {
+    // Subscribe to route changes
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Check the current route
+        this.showNavbar = !['/login', '/register', '/forgot-password'].includes(event.url);
+      }
+    });
+  }
 }
