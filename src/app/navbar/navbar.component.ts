ac1300b30de06acd1fc892351b326cfa239e5c5c
Lyne986 <71839652+Lyne986@users.noreply.github.com>
2023-12-04 00:46:19 +0900

    [REMOVED] Useless code in navbar.components.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,31 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-  admin: boolean = false;
-  // Add the following properties and methods
-  menuOpen: boolean = false;
+export class NavbarComponent {
 
-  goto(route: string) {
-    // Implement your navigation logic here
-    console.log(`Navigating to ${route}`);
-  }
 
-  logout() {
-    // Implement your logout logic here
-    console.log('Logging out');
-  }
-
-  toggleMenu() {
-    // Implement your menu toggle logic here
-    this.menuOpen = !this.menuOpen;
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
-    // Add any initialization logic here
-  }
-
-  // ... rest of your code
 }
